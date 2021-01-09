33d0d4fede9f58c6d1b6d61020acd60a719a7e84
TonyJiangWJ <tonyjiangwj@gmail.com>
2021-01-09 20:13:13 +0800

    同步更新代码,优化currentPackage

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: TonyJiangWJ
  * @Last Modified by: TonyJiangWJ
- * @Last Modified time: 2020-12-26 11:28:51
+ * @Last Modified time: 2021-01-09 14:24:42
  * @Description: 
  */
 require('./modules/init_if_needed.js')(runtime, this)
@@ -26,19 +26,20 @@ logInfo('======加入任务队列，并关闭重复运行的脚本=======')
 runningQueueDispatcher.addRunningTask()
 // 注册自动移除运行中任务
 commonFunctions.registerOnEngineRemoved(function () {
+  config.resetBrightness && config.resetBrightness()
+  events.removeAllListeners()
+  events.recycle()
+  debugInfo('校验并移除已加载的dex')
+  resolver()
+  flushAllLogs()
+  // 针对免费版内存主动释放，Pro版不需要
+  !config.is_pro && console.clear()
   // 移除运行中任务
   runningQueueDispatcher.removeRunningTask(true, true,
     () => {
       // 保存是否需要重新锁屏
       unlocker.saveNeedRelock()
-      unlocker.unlocker.relock && _config.resetBrightness && _config.resetBrightness()
-      events.removeAllListeners()
-      events.recycle()
-      debugInfo('校验并移除已加载的dex')
-      resolver()
-      flushAllLogs()
       config.isRunning = false
-      console.clear()
     }
   )
 }, 'main')
